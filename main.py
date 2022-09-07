b2563030d3ed7153aab0fabefd1db2cfceb80956
Ishaan Rao <ishaanrao@Ishaans-MacBook-Air.local>
2022-09-07 19:06:16 -0400

    Imported pygame module and defined 4 basic functions, addition, subtraction, mulitplication, and division.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Import modules
+import pygame
+
+# Create calculating functions
+def addition(*x):
+    pass
+
+def subtraction(*x):
+    pass
+
+def multiplication(*x):
+    pass
+
+def division(*x):
+    pass
+
+
+# Create GUI
